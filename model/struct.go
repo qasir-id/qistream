1da25be79cc4cd84f108f6a278d1df7055bcc36c
Luqmanul Hakim <n.loekman@gmail.com>
2020-05-04 14:10:48 +0700

    add atribute field in log

--- model/struct.go
+++ model/struct.go
@@ -6,6 +6,7 @@ type PubSubMessageLog struct {
 	ID             int64      `json:"id" gorm:"id"`
 	PubsubID       string     `json:"pubsub_id" gorm:"pubsub_id"`
 	Data           string     `json:"data" gorm:"data"`
+	Attribute      string     `json:"attribute" gorm:"attribute"`
 	PublishTime    *time.Time `json:"publish_time" gorm:"publish_time"`
 	ReceiveTime    *time.Time `json:"receive_time" gorm:"receive_time"`
 	CreatedAt      time.Time  `json:"created_at" gorm:"created_at"`
